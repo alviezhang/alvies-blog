dfa3a82b9e3ad3123d954373ea3daaed02556a0c
Alvie Zhang <alvie.zhang@gmail.com>
2021-07-01 16:35:33 +0800

    Footer add link

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,10 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+            {` `} ♥ {` `} <a href="https://dahan.io">Han</a>
       </footer>
     </div>
   )
